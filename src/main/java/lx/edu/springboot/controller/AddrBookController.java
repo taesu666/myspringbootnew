c05b2eb55b83826ff1b8144749da37e8e1fd0352
taesu666 <wmiso.j@gmail.ocm>
2025-09-10 14:05:38 +0900

    feat/edit

--- src/main/java/lx/edu/springboot/controller/AddrBookController.java
+++ src/main/java/lx/edu/springboot/controller/AddrBookController.java
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -16,58 +15,45 @@ import lx.edu.springboot.vo.AddrBookVO;
 
 @Controller
 public class AddrBookController {
-	
 	@Autowired
 	ApplicationContext context;
-	
+
 	@Autowired
 	AddrBookDAO dao;
+
+	@RequestMapping("/insert.do")
+	public String insert(AddrBookVO vo) throws Exception {
+		System.out.println(vo);
+		dao.insertDB(vo);
+		return "redirect:addrbook_list.do";
+	}
 
 	@RequestMapping("/addrbook_form.do")
 	public String form() {
 		return "addrbook_form";//jsp file name
 	}
-
 	
-	@RequestMapping("/addrbook_list.do")
+	@RequestMapping("addrbook_list.do")
 	public String list(HttpSession session, HttpServletRequest req) throws Exception {
+//		if(session.getAttribute("userId")==null ) {
+//			return "login";
+//		}
 		List<AddrBookVO> list = dao.getDBList();
 		req.setAttribute("data", list);
 		return "addrbook_list";
 	}
 	
-	
 	@RequestMapping("/edit.do")
-	public String edit(@RequestParam("abId") int abId ,HttpServletRequest req) throws Exception {
-		
-		AddrBookVO vo = dao.getDB(abId);
+	public String edit(@RequestParam("abId") int abId, HttpServletRequest req) throws Exception {
+		AddrBookVO vo = dao.selectDBList(abId);
 		req.setAttribute("ab", vo);
-		return "addrbook_edit_form";//jsp file name
+		return "addrbook_edit_form";
 	}
 	
 	@RequestMapping("/update.do")
-	public String update(AddrBookVO vo) {
+	public String update(AddrBookVO vo) throws Exception {
 		System.out.println("vo=" + vo);
-		//dao.updateDB(vo);
+		//dao.update(vo);
 		return "redirect:addrbook_list.do";
 	}
-	
-	@RequestMapping("/addrbook_editlist.do")
-	public String editlist(AddrBookVO ab) throws Exception {
-		System.out.println(ab);
-		dao.updateDB(ab);
-		
-		return "redirect:addrbook_list.do";
-		
-	}
-	
-	@RequestMapping("/addrbook_insert.do")
-	public String insert(AddrBookVO vo) throws Exception {
-		//1. 클라이언트가 보낸 정보 꺼내오기
-		System.out.println(vo);
-		//2. dao 이용해 전달받은 거 보내줌
-		dao.insertDB(vo);
-		return "redirect:addrbook_list.do";
-	}
-	
 }
